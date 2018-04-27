94bfe8e74769b8bc9658ed4355b7c31a7b97b084
Dionisius Mayr <dionisiusom@gmail.com>
2018-04-26 22:06:26 -0300

    Add every file

--- src/Opcodes.cpp
+++ src/Opcodes.cpp
@@ -8,6 +8,9 @@ namespace opc {
 
     void print_inst(uint8_t const opcode) {
         switch(opcode) {
+            case 0x69: case 0x65: case 0x75: case 0x6D: case 0x7D: case 0x79:
+            case 0x61: case 0x71:
+                _print_inst("ADC", opcode);
             case 0x00: _print_inst("BRK", opcode);
             case 0x04: _print_inst("NOP", opcode);
         }
